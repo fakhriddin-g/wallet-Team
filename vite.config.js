b43b2ae2b1db71c5043276e58335644c4c68dbaa
Daler <dsarifkulo021@gmail.com>
2023-06-06 17:24:31 +0500

    testing pages

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+// vite.config.js
+import {
+    resolve
+} from 'path'
+import {
+    defineConfig
+} from 'vite'
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                about: resolve(__dirname, 'pages/about/index.html'),
+                catalog: resolve(__dirname, 'pages/catalog/index.html')
+            },
+        },
+    },
+})
