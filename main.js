d74748f27a7ec5ad09ac13f4c5a2c2a8b4481719
Amal <amalabdurahmonov427@gmail.com>
2023-06-08 13:24:18 +0300

    first

---
+++ main.js
@@ -0,0 +1,91 @@
+let baseUrl = "http://localhost:5050"
+let form = document.forms.form
+
+
+const getUser = () => {
+    fetch(baseUrl + "/users")
+        .then(res => res.json())
+}
+
+getUser()
+
+let inps = document.querySelectorAll('input')
+
+inps.forEach((inp) => {
+    inp.onkeyup = () => {
+        let key = inp.name
+        patterns[key]
+        validate(patterns[key], inp)
+    }
+})
+
+let patterns = {
+    name: /^[a-z ,.'-]+$/i,
+    surname: /^[a-z ,.'-]+$/i,
+    email: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+    password: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*(\W|_)).{5,}$/
+}
+
+function validate(regex, field) {
+    if (regex.test(field.value) || field.value.length > 3) {
+        field.classList.remove('error')
+    } else {
+        field.classList.add('error')
+    }
+}
+
+form.onsubmit = (e) => {
+    e.preventDefault();
+
+    let isFormValid = true;
+
+    inps.forEach(inp => {
+        if (inp.value.length < 3) {
+            inp.classList.add('error');
+            isFormValid = false;
+        } else {
+            inp.classList.remove('error');
+        }
+    });
+
+    if (isFormValid) {
+        let userInfo = {
+            id: Math.random()
+        }
+
+        let formData = new FormData(form);
+
+        for (let [key, value] of formData.entries()) {
+            userInfo[key] = value;
+        }
+
+        localStorage.setItem("user", JSON.stringify(userInfo))
+
+        location.assign("pages/about/")
+
+        let json = JSON.stringify(userInfo);
+
+        creatUser(json);
+
+        form.reset();
+
+        location.assign("/pages/about/")
+    }
+}
+
+
+
+
+
+const creatUser = async (body) => {
+    const res = await fetch(baseUrl + "/users", {
+        method: "post",
+        body: body,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    if (res.status === 201 || res.status === 200) {
+        getUser()
+    }
+}
---
+++ pages/about/script.js
@@ -0,0 +1,67 @@
+let baseUrl = "http://localhost:5050"
+
+let inpEmail = document.querySelector('.inp-email')
+let inpPas = document.querySelector('.inp-pass')
+let form = document.forms.log_in
+
+let userData = JSON.parse(localStorage.getItem("user"))
+
+const getUser = () => {
+    fetch(baseUrl + "/users")
+        .then(res => res.json())
+}
+
+// inpEmail.value = userData.email
+
+
+let inps = document.querySelectorAll('input')
+
+
+inps.forEach((inp) => {
+    inp.onkeyup = () => {
+        let key = inp.name
+        patterns[key]
+        validate(patterns[key], inp)
+    }
+})
+
+
+let patterns = {
+    email: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+    password: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*(\W|_)).{5,}$/
+}
+
+function validate(regex, field) {
+    if (regex.test(field.value) || field.value.length > 3) {
+        field.classList.remove('error')
+    } else {
+        field.classList.add('error')
+    }
+}
+
+form.onsubmit = (e) => {
+    e.preventDefault();
+
+
+    if(userData.password == inpPas.value) {
+            let user = {
+                id: Math.random()
+            }
+    
+            let fm = new FormData(form)
+        
+            fm.forEach((value, key) => {
+                user[key] = value
+            })
+        
+        
+            location.assign("/")
+        
+            form.reset()
+        
+        
+            console.log(user);
+        
+    }
+
+}
