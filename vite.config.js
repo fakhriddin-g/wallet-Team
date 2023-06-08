7dedd2f3d37e118f6b8412d50a7227588dbab834
Fakhriddin <fakhmirakov1998@gmail.com>
2023-06-08 13:02:01 +0500

    start project

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,20 @@
 // vite.config.js
 import {
-    resolve
+  resolve
 } from 'path'
 import {
-    defineConfig
+  defineConfig
 } from 'vite'
 
 export default defineConfig({
-    build: {
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, 'index.html'),
-                about: resolve(__dirname, 'pages/about/index.html'),
-                catalog: resolve(__dirname, 'pages/catalog/index.html'),
-                transactions: resolve(__dirname, 'pages/transactions/index.html'),
-            },
-        },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'pages/about/index.html'),
+        catalog: resolve(__dirname, 'pages/catalog/index.html'),
+        transactions: resolve(__dirname, 'pages/transactions/index.html'),
+      },
     },
+  },
 })
